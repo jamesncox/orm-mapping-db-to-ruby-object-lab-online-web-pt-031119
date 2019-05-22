5a06b99a25c4165a8bb0c5ab98774241afe6db36
James Cox <jamesncox@yahoo.com>
2019-05-22 14:25:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,6 +65,12 @@ class Student
   end
   
   def self.all_students_in_grade_9
+    sql = <<-SQL 
+      SELECT *
+      FROM students
+      WHERE grade = 9 
+    SQL
     
+    DB[:conn].execute
   end 
 end
