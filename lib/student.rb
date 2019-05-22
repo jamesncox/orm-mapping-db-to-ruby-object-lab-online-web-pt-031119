8fd25d4080c3925581b1828ae55c4be1d75bc80f
James Cox <jamesncox@yahoo.com>
2019-05-22 14:48:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -124,7 +124,7 @@ class Student
       ORDER BY students.id
     SQL
     
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, grade).map do |row|
       self.new_from_db(row)
     end
   end 
