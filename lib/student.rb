3a35a5c5841a8e91016551ab9b4be2870b07e1aa
James Cox <jamesncox@yahoo.com>
2019-05-22 14:38:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -97,7 +97,7 @@ class Student
       LIMIT ?
     SQL
     
-    DB[:conn].execute(sql, grade).map do |row|
+    DB[:conn].execute(sql, number).map do |row|
       self.new_from_db(row)
     end
   end
